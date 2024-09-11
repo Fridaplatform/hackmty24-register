22b1d4503341f8aa8e651933f3468d01e08b27a2
Jdelbarcogarza <jdelbarcogarza@gmail.com>
2024-09-11 11:39:20 -0600

    git init

---
+++ firebase/index.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5EqBo5wOAAtFsNt-hmj1SN7ayDgy8V3I",
+  authDomain: "hackmty24-signup.firebaseapp.com",
+  projectId: "hackmty24-signup",
+  storageBucket: "hackmty24-signup.appspot.com",
+  messagingSenderId: "259695399230",
+  appId: "1:259695399230:web:2d3e75150f1f9593525c6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app }
